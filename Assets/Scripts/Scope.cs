29d6b77686a08824eed8a0f6671d171fa245214b
Christian Jescke <chrisjberlin@googlemail.com>
2021-03-16 23:23:39 +0800

    add scope when zoom out

--- Assets/Scripts/Scope.cs
+++ Assets/Scripts/Scope.cs
@@ -8,41 +8,44 @@ public class Scope : MonoBehaviour
     bool scopeEnabled = false;
     Vector3 mousePos = Vector3.zero;
     Vector3 touchPosition = Vector3.zero;
-    // Start is called before the first frame update
+    Vector3 mousePosition = Vector3.zero;
     void Start()
     {
         Cursor.visible = false;
-
         zoomChangeEvent.OnZoomChange += ZoomByControl_OnZoomChange;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (scopeEnabled)
-        {
-            if (Input.touchCount >= 1)
-            {
-                touchPosition = Camera.main.ScreenToWorldPoint(Input.touches[0].position);
-                transform.position = new Vector3(touchPosition.x, touchPosition.y, 0f);
-            }
-            else if (Input.GetMouseButton(0))
-            {
 
-                Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                transform.position = new Vector3(mousePosition.x, mousePosition.y, 0);
-            }
+        if (Input.touchCount >= 1)
+        {
+            touchPosition = Camera.main.ScreenToWorldPoint(Input.touches[0].position);
+            mousePosition = Camera.main.ScreenToWorldPoint(touchPosition);
         }
+        else if (SystemInfo.deviceType != DeviceType.Handheld) { 
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        transform.position = new Vector3(mousePosition.x, mousePosition.y, 0);
+            
+        
     }
     private void ZoomByControl_OnZoomChange(object sender, ZoomByControl.OnZoomChangeEventArgs e)
     {
         if (e.In)
         {
             scopeEnabled = true;
+            this.GetComponent<SpriteRenderer>().sortingOrder = 2;
+            this.GetComponent<SpriteRenderer>().maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
+            this.transform.localScale = new Vector3(1, 1, 1);
         }
         else
         {
             scopeEnabled = false;
+            this.GetComponent<SpriteRenderer>().sortingOrder = 5;
+            this.GetComponent<SpriteRenderer>().maskInteraction = SpriteMaskInteraction.None;
+            this.transform.localScale = new Vector3(2, 2, 2);
         }
     }
     private void OnDestroy()
